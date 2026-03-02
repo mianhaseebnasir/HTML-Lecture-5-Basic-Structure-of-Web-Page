23850f09b4f11afe13298951f19b8399732a611f
Mian Haseeb Nasir <mianhaseebnasir1@gmail.com>
2026-03-02 05:20:13 +0500

    Add files via upload

---
+++ Lecture-5 (JS)/index.js
@@ -0,0 +1,37 @@
+// alert 1.
+alert("Mian Haseeb Nasir");
+
+// console(Browser Tool) + log. 
+console.log("Hello World");
+
+// alert 2.
+alert("804");
+
+// alert 3.
+alert("true");
+
+// document.writeIn means wrie the sentence in web-page.
+document.writeln("Hi, Mian Haseeb Nasir");
+
+// console(Browser Tool) + log.
+console.log("Hi, Mian Haseeb Nasir ");
+
+// a and b is a variable + prompt is like input.
+a = prompt("Enter Your 1st Number");
+b = prompt("Enter Your 2st Number");
+
+// alert 4.
+alert(a+b);
+
+// console(Browser Tool) + log.  
+console.log(a+b);
+
+// document.writeIn means wrie the sentence in web-page.
+document.writeln(a+b);
+
+// dataTypes.
+// int   (int means intergers may be positive or negative like:786,-804).
+// float (decimal numbers like:2.222223).
+// string ("Hello, world!").
+// bigInt (like:2.23333343e , e means exponential).
+// boolean (true or false).
